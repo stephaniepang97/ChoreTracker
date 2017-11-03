f2671ccff1e3b0b62ca7345eafb19f43f4cf968a
Stephanie Pang <spang@andrew.cmu.edu>
2017-11-03 00:37:41 +0000

    Chores model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,20 @@
 class Chore < ApplicationRecord
+    belongs_to :child
+    belongs_to :task
+    
+    validates_date :due_on
+    
+    scope :by_task, -> { joins(:task).order('tasks.name') }
+    scope :chronological, -> { order(:due_on) }
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where("due_on >= ?", Date.current) }
+    scope :past, -> { where("due_on < ?", Date.current) }
+    
+    def status
+        if completed
+            return "Completed"
+        end
+        return "Pending"
+    end
 end
